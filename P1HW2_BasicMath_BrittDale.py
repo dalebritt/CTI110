1a32636959af9a29aaf4b2c50498a6da453591df
dalebritt <71046291+dalebritt@users.noreply.github.com>
2020-09-12 17:15:40 -0400

    P1HW2_BasicMath
    P1HW2_BasicMath

---
+++ P1HW2_BasicMath_BrittDale.py
@@ -0,0 +1,15 @@
+# Module 2 Homework 2
+# 09/12/2020
+# CTI-110 P1HW2 - Basic Math
+# Dale Britt
+#
+# Receive two numbers from user input
+first_number = int(input('Please Enter first number: '))
+second_number = int(input('Please Enter second number: '))
+# Output of users first and second number
+print('First number entered:', first_number)
+print('Second number entered:', second_number)
+print()
+# Users input for first and second number added and multiplied
+print('Sum of both numbers:', first_number + second_number)
+print('Result of Multiplying the two numbers:', first_number * second_number)
