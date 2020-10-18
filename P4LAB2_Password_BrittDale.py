2c6d84f0f9dfcb07538f48650313ebec730dc4f7
dalebritt <71046291+dalebritt@users.noreply.github.com>
2020-10-18 18:31:57 -0400

    P4LAB2_Password_BrittDale
    P4LAB2_Password_BrittDale

---
+++ P4LAB2_Password_BrittDale.py
@@ -0,0 +1,20 @@
+word = input()
+password = ''
+
+for character in word:
+    if character == 'i':
+        password += '!'
+    elif character == 'a':
+        password += '@'
+    elif character == 'm':
+        password += 'M'
+    elif character == 'B':
+        password += '8'
+    elif character == 'o':
+        password += '.'
+    else:
+        password += character
+
+password += 'q*s'
+print(password)
+    
